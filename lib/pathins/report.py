6b9cf7dd4debeab66f07825fdc1984defc682bd4
Chris Simpkins <git.simpkins@gmail.com>
2020-09-29 01:21:56 -0400

    initial implementation report module

---
+++ lib/pathins/report.py
@@ -0,0 +1,28 @@
+import argparse
+
+from fontTools.ttLib import TTFont  # type: ignore
+
+from .bridge import ttfont_glyph_to_skia_path
+from .stringbuilder import path_header
+from .validators import validate_fontpath, validate_glyph_in_font
+
+
+def report_run(args: argparse.Namespace) -> None:
+    fontpath: str = args.fontpath
+    glyphname: str = args.glyphname
+
+    # --------------------
+    # CLI arg validations
+    # --------------------
+    validate_fontpath(fontpath)
+
+    tt = TTFont(fontpath)
+
+    # confirm that `glyphname` request is in the font
+    validate_glyph_in_font(glyphname, tt)
+
+    skia_path = ttfont_glyph_to_skia_path(glyphname, tt)
+
+    # Path dump from skia
+    print(path_header(f"'{glyphname}' path"))
+    skia_path.dump()
