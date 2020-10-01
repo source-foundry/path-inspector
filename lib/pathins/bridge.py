25629991021020f2aca335710e30fd22a5f66dbc
Chris Simpkins <git.simpkins@gmail.com>
2020-09-30 20:18:39 -0400

    add docstring documentation

--- lib/pathins/bridge.py
+++ lib/pathins/bridge.py
@@ -6,6 +6,11 @@ from fontTools.ttLib.tables import _g_l_y_f  # type: ignore
 
 
 def ttfont_glyph_to_skia_path(glyph_name: str, tt_font: ttFont.TTFont) -> pathops.Path:
+    """
+    Converts fontTools.ttLib.TTFont glyph to a pathops.Path object
+    by glyph name.  During this conversion, all composite paths are
+    decomposed.
+    """
     glyf_table = tt_font["glyf"]
     glyph_set: ttFont._TTGlyphSet = tt_font.getGlyphSet()
     tt_glyph = glyf_table[glyph_name]
@@ -23,9 +28,12 @@ def ttfont_glyph_to_skia_path(glyph_name: str, tt_font: ttFont.TTFont) -> pathop
 
 
 def skia_path_to_ttfont_glyph(skia_path: pathops.Path) -> _g_l_y_f.Glyph:
+    """
+    Converts a pathops.Path object to a fontTools.ttLib._g_l_y_f.Glyph
+    object
+    """
     tt_pen = TTGlyphPen(glyphSet=None)
     skia_path.draw(tt_pen)
     glyph = tt_pen.glyph()
-    assert not glyph.isComposite()
     glyph.recalcBounds(glyfTable=None)
     return glyph
