815ae8b7d73517108936ae866817b2b9c5a739b6
Chris Simpkins <git.simpkins@gmail.com>
2020-09-30 23:01:50 -0400

    [path] add no contour support

--- lib/pathins/path.py
+++ lib/pathins/path.py
@@ -9,6 +9,12 @@ from .validators import validate_fontpath, validate_glyph_in_font
 
 
 def path_run(args: argparse.Namespace) -> None:
+    """
+    Parses command line arguments to the `path` sub-command
+    and dumps glyph-level path reports to standard output
+    stream for entire glyph set or by command line specified
+    glyph name.
+    """
     fontpath: str = args.fontpath
     glyphname: str = args.glyphname
 
@@ -27,14 +33,20 @@ def path_run(args: argparse.Namespace) -> None:
 
         skia_path = ttfont_glyph_to_skia_path(glyphname, tt)
         print(path_header(f"'{glyphname}' path", nocolor=args.nocolor))
-        print(skia_path)
+        if len(list(skia_path.contours)) == 0:
+            print("No contours")
+        else:
+            print(skia_path)
     else:
         glyph_names = tt.getGlyphOrder()
         len_glyph_names = len(glyph_names)
         for x, local_glyphname in enumerate(glyph_names):
             skia_path = ttfont_glyph_to_skia_path(local_glyphname, tt)
             print(path_header(f"'{local_glyphname}' path", nocolor=args.nocolor))
-            print(skia_path)
+            if len(list(skia_path.contours)) == 0:
+                print("No contours")
+            else:
+                print(skia_path)
             if x + 1 < len_glyph_names:
                 # append a newline to all glyph reports except last
                 print("")
