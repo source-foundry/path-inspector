56eb023550c7dfe5553e52c4257eda93cc2f7e45
Chris Simpkins <git.simpkins@gmail.com>
2020-10-02 19:17:21 -0400

    add multi-glyph coordinates unit tests

--- tests/test_coordinates.py
+++ tests/test_coordinates.py
@@ -146,14 +146,14 @@ def test_coordinates_run_single_glyph_composite_default(capsys, monkeypatch):
     assert f"(303, 201) {COLORED_START}" in captured.out
     assert f"(303, 228) {ON_PATH}" in captured.out
 
-    assert f"(303, 266)" in captured.out
+    assert "(303, 266)" in captured.out
     assert f"(303, 266) {ON_PATH}" not in captured.out
 
     assert f"(233, 201) {COLORED_END}" in captured.out
 
     assert f"(326, 54) {COLORED_START}" in captured.out
 
-    assert f"(326, 91)" in captured.out
+    assert "(326, 91)" in captured.out
     assert f"(326, 91) {ON_PATH}" not in captured.out
 
     assert f"(264, -14) {ON_PATH}" in captured.out
@@ -180,16 +180,175 @@ def test_coordinates_run_single_glyph_composite_nocolor(capsys, monkeypatch):
     assert f"(303, 201) {UNCOLORED_START}" in captured.out
     assert f"(303, 228) {ON_PATH}" in captured.out
 
-    assert f"(303, 266)" in captured.out
+    assert "(303, 266)" in captured.out
     assert f"(303, 266) {ON_PATH}" not in captured.out
 
     assert f"(233, 201) {UNCOLORED_END}" in captured.out
 
     assert f"(326, 54) {UNCOLORED_START}" in captured.out
 
-    assert f"(326, 91)" in captured.out
+    assert "(326, 91)" in captured.out
     assert f"(326, 91) {ON_PATH}" not in captured.out
 
     assert f"(264, -14) {ON_PATH}" in captured.out
 
     assert f"(326, 18) {UNCOLORED_END}" in captured.out
+
+
+def test_coordinates_run_multi_glyph_default(capsys, monkeypatch):
+    def mock_isatty():
+        return True
+
+    # apply the monkeypatch for sys.stdout.isatty()
+    monkeypatch.setattr(sys.stdout, "isatty", mock_isatty)
+
+    args = parser.parse_args([TESTFONT_PATH_1])
+    coordinates_run(args)
+
+    captured = capsys.readouterr()
+
+    # confirm all expected glyphs are included
+    assert "\x1b[1;36m'.notdef' coordinates\x1b[0m" in captured.out
+    assert "\x1b[1;36m'space' coordinates\x1b[0m" in captured.out
+    assert "\x1b[1;36m'comma' coordinates\x1b[0m" in captured.out
+    assert "\x1b[1;36m'question' coordinates\x1b[0m" in captured.out
+    assert "\x1b[1;36m'A' coordinates\x1b[0m" in captured.out
+    assert "\x1b[1;36m'uni2E2E' coordinates\x1b[0m" in captured.out
+
+    # check start paths across the glyphs
+    assert f"(185, 116) {COLORED_START}" in captured.out
+    assert f"(140, 201) {COLORED_START}" in captured.out
+    assert f"(117, 54) {COLORED_START}" in captured.out
+    assert f"(545, 0) {COLORED_START}" in captured.out
+    assert f"(432, 301) {COLORED_START}" in captured.out
+    assert f"(303, 201) {COLORED_START}" in captured.out
+    assert f"(326, 54) {COLORED_START}" in captured.out
+
+    # check end paths across the glyphs
+    assert f"(91, 116) {COLORED_END}" in captured.out
+    assert f"(210, 201) {COLORED_END}" in captured.out
+    assert f"(117, 18) {COLORED_END}" in captured.out
+    assert f"(638, 0) {COLORED_END}" in captured.out
+    assert f"(206, 301) {COLORED_END}" in captured.out
+    assert f"(233, 201) {COLORED_END}" in captured.out
+    assert f"(326, 18) {COLORED_END}" in captured.out
+
+    # check glyphs with no contours across the glyphs
+    x = 1
+    lines = captured.out.split("\n")
+    for line in lines:
+        if x == 4 or x == 9:
+            assert "No contours" in line
+        x += 1
+
+    # spot check on-path coordinates in noncomposite glyphs
+    assert f"(140, 228) {ON_PATH}" in captured.out
+    assert f"(352, 517) {ON_PATH}" in captured.out
+    assert f"(240, 54) {ON_PATH}" in captured.out
+
+    # spot check off-path coordinates in noncomposite glyphs
+    assert f"(130, -75) {ON_PATH}" not in captured.out
+    assert "(130, -75)" in captured.out
+
+    assert f"(210, 246) {ON_PATH}" not in captured.out
+    assert "(210, 246)" in captured.out
+
+    assert f"(322, 612) {ON_PATH}" not in captured.out
+    assert "(322, 612)" in captured.out
+
+    assert f"(251, 370) {ON_PATH}" not in captured.out
+    assert "(251, 370)" in captured.out
+
+    assert f"(203, 91) {ON_PATH}" not in captured.out
+    assert "(203, 91)" in captured.out
+
+
+def test_coordinates_run_multi_glyph_nocolor(capsys, monkeypatch):
+    def mock_isatty():
+        return True
+
+    # apply the monkeypatch for sys.stdout.isatty()
+    monkeypatch.setattr(sys.stdout, "isatty", mock_isatty)
+
+    args = parser.parse_args(["--nocolor", TESTFONT_PATH_1])
+    coordinates_run(args)
+
+    captured = capsys.readouterr()
+
+    # confirm all expected glyphs are included
+    assert "\x1b[1;36m'.notdef' coordinates\x1b[0m" not in captured.out
+    assert "\x1b[1;36m'space' coordinates\x1b[0m" not in captured.out
+    assert "\x1b[1;36m'comma' coordinates\x1b[0m" not in captured.out
+    assert "\x1b[1;36m'question' coordinates\x1b[0m" not in captured.out
+    assert "\x1b[1;36m'A' coordinates\x1b[0m" not in captured.out
+    assert "\x1b[1;36m'uni2E2E' coordinates\x1b[0m" not in captured.out
+
+    assert "'.notdef' coordinates" in captured.out
+    assert "'space' coordinates" in captured.out
+    assert "'comma' coordinates" in captured.out
+    assert "'question' coordinates" in captured.out
+    assert "'A' coordinates" in captured.out
+    assert "'uni2E2E' coordinates" in captured.out
+
+    # check start paths across the glyphs
+    assert f"(185, 116) {COLORED_START}" not in captured.out
+    assert f"(140, 201) {COLORED_START}" not in captured.out
+    assert f"(117, 54) {COLORED_START}" not in captured.out
+    assert f"(545, 0) {COLORED_START}" not in captured.out
+    assert f"(432, 301) {COLORED_START}" not in captured.out
+    assert f"(303, 201) {COLORED_START}" not in captured.out
+    assert f"(326, 54) {COLORED_START}" not in captured.out
+
+    assert f"(185, 116) {UNCOLORED_START}" in captured.out
+    assert f"(140, 201) {UNCOLORED_START}" in captured.out
+    assert f"(117, 54) {UNCOLORED_START}" in captured.out
+    assert f"(545, 0) {UNCOLORED_START}" in captured.out
+    assert f"(432, 301) {UNCOLORED_START}" in captured.out
+    assert f"(303, 201) {UNCOLORED_START}" in captured.out
+    assert f"(326, 54) {UNCOLORED_START}" in captured.out
+
+    # check end paths across the glyphs
+    assert f"(91, 116) {COLORED_END}" not in captured.out
+    assert f"(210, 201) {COLORED_END}" not in captured.out
+    assert f"(117, 18) {COLORED_END}" not in captured.out
+    assert f"(638, 0) {COLORED_END}" not in captured.out
+    assert f"(206, 301) {COLORED_END}" not in captured.out
+    assert f"(233, 201) {COLORED_END}" not in captured.out
+    assert f"(326, 18) {COLORED_END}" not in captured.out
+
+    assert f"(91, 116) {UNCOLORED_END}" in captured.out
+    assert f"(210, 201) {UNCOLORED_END}" in captured.out
+    assert f"(117, 18) {UNCOLORED_END}" in captured.out
+    assert f"(638, 0) {UNCOLORED_END}" in captured.out
+    assert f"(206, 301) {UNCOLORED_END}" in captured.out
+    assert f"(233, 201) {UNCOLORED_END}" in captured.out
+    assert f"(326, 18) {UNCOLORED_END}" in captured.out
+
+    # check glyphs with no contours across the glyphs
+    x = 1
+    lines = captured.out.split("\n")
+    for line in lines:
+        if x == 4 or x == 9:
+            assert "No contours" in line
+        x += 1
+
+    # spot check on-path coordinates in noncomposite glyphs
+    assert f"(140, 228) {ON_PATH}" in captured.out
+    assert f"(352, 517) {ON_PATH}" in captured.out
+    assert f"(240, 54) {ON_PATH}" in captured.out
+
+    # spot check off-path coordinates in noncomposite glyphs
+    assert f"(130, -75) {ON_PATH}" not in captured.out
+    assert "(130, -75)" in captured.out
+
+    assert f"(210, 246) {ON_PATH}" not in captured.out
+    assert "(210, 246)" in captured.out
+
+    assert f"(322, 612) {ON_PATH}" not in captured.out
+    assert "(322, 612)" in captured.out
+
+    assert f"(251, 370) {ON_PATH}" not in captured.out
+    assert "(251, 370)" in captured.out
+
+    assert f"(203, 91) {ON_PATH}" not in captured.out
+    assert "(203, 91)" in captured.out
