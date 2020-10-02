5a5692ffe22678222c395a4531b62761cf332c60
Chris Simpkins <git.simpkins@gmail.com>
2020-10-02 11:35:38 -0400

    [coordinates] decompose composite glyphs in single glyph requests
    addresses bug: composite glyphs reported as having no contours and therefore no coordinates, must decompose first

--- lib/pathins/coordinates.py
+++ lib/pathins/coordinates.py
@@ -11,6 +11,7 @@ from .stringbuilder import green_text, red_text, report_header
 from .validators import validate_fontpath, validate_glyph_in_font
 
 FLAG_ON_CURVE = 0x01
+
 ON_OFF = ["", "----- on -----"]
 START_STRING = "START ~~~~~~~~"
 END_STRING = "~~~~~~~~~~ END"
@@ -41,6 +42,12 @@ def coordinates_run(args: argparse.Namespace) -> None:
 
         glyph = glyf_table[glyphname]
 
+        # decompose composite glyphs
+        if glyph.isComposite():
+            glyph = skia_path_to_ttfont_glyph(
+                ttfont_glyph_to_skia_path(local_glyphname, tt)
+            )
+
         print(report_header(f"'{glyphname}' coordinates", nocolor=args.nocolor))
         sys.stdout.write(
             coordinates_report(glyphname, glyph, glyf_table, nocolor=args.nocolor)
@@ -51,14 +58,13 @@ def coordinates_run(args: argparse.Namespace) -> None:
         for x, local_glyphname in enumerate(glyph_names):
             glyph = glyf_table[local_glyphname]
 
+            # decompose composite glyphs
             if glyph.isComposite():
                 glyph = skia_path_to_ttfont_glyph(
                     ttfont_glyph_to_skia_path(local_glyphname, tt)
                 )
 
-            print(
-                report_header(f"'{local_glyphname}' coordinates", nocolor=args.nocolor)
-            )
+            print(report_header(f"'{local_glyphname}' coordinates", nocolor=args.nocolor))
             sys.stdout.write(
                 coordinates_report(
                     local_glyphname, glyph, glyf_table, nocolor=args.nocolor
@@ -83,13 +89,26 @@ def coordinates_report(
 
         coordinates_string: str = ""
         for x, coord in enumerate(coords):
+            # on- and off-curve points are defined in
+            # the `flags` integer array that are mapped
+            # 1:1 to coordinate indices
+            # test the on curve bit mask here and
+            # use this to define the appropriate
+            # string value in the report
             on_off = ON_OFF[flags[x] & FLAG_ON_CURVE]
+            # this is a start coordinate if it
+            # (1) is the first coordinate in the iterable
+            # (2) follows a previous endpoint coordinate
             start_coord = (x == 0) or (coords[x - 1] in endpoint_coordinates)
             if start_coord:
                 coordinates_string += (
                     f"{str(coord): >13} "
                     f"{green_text(START_STRING, nocolor=nocolor): <13}{os.linesep}"
                 )
+            # end coordinates are defined by the indices returned
+            # in the Glyph.getGlyphCoordinates method return tuple
+            # compare current test coordinate with those coordinate
+            # values
             elif coords[x] in endpoint_coordinates:
                 coordinates_string += (
                     f"{str(coord): >13} "
