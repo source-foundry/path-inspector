d79c1664e801271ab75a6d3d082d0b9707f8c4e2
Chris Simpkins <git.simpkins@gmail.com>
2020-10-01 21:09:51 -0400

    [path] transition to refactored report_header function

--- lib/pathins/path.py
+++ lib/pathins/path.py
@@ -4,7 +4,7 @@ import pathops  # type: ignore
 from fontTools.ttLib import TTFont  # type: ignore
 
 from .bridge import ttfont_glyph_to_skia_path
-from .stringbuilder import path_header
+from .stringbuilder import report_header
 from .validators import validate_fontpath, validate_glyph_in_font
 
 
@@ -32,7 +32,7 @@ def path_run(args: argparse.Namespace) -> None:
         validate_glyph_in_font(glyphname, tt)
 
         skia_path = ttfont_glyph_to_skia_path(glyphname, tt)
-        print(path_header(f"'{glyphname}' path", nocolor=args.nocolor))
+        print(report_header(f"'{glyphname}' path", nocolor=args.nocolor))
         if len(list(skia_path.contours)) == 0:
             print("No contours")
         else:
@@ -42,7 +42,7 @@ def path_run(args: argparse.Namespace) -> None:
         len_glyph_names = len(glyph_names)
         for x, local_glyphname in enumerate(glyph_names):
             skia_path = ttfont_glyph_to_skia_path(local_glyphname, tt)
-            print(path_header(f"'{local_glyphname}' path", nocolor=args.nocolor))
+            print(report_header(f"'{local_glyphname}' path", nocolor=args.nocolor))
             if len(list(skia_path.contours)) == 0:
                 print("No contours")
             else:
