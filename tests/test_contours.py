8095c8ae137d342424d6db21fa0793b5b096236d
Chris Simpkins <git.simpkins@gmail.com>
2020-10-03 12:14:40 -0400

    add contours multi-glyph unit tests

--- tests/test_contours.py
+++ tests/test_contours.py
@@ -139,3 +139,45 @@ def test_contours_run_single_glyph_composite_nocolor(capsys, monkeypatch):
     # must be in a tty to get ANSI color output
     # this is mocked above
     assert "[ uni2E2E ]: 2" in captured.out
+
+
+def test_contours_run_multi_glyph_composite_default(capsys, monkeypatch):
+    def mock_isatty():
+        return True
+
+    # apply the monkeypatch for sys.stdout.isatty()
+    monkeypatch.setattr(sys.stdout, "isatty", mock_isatty)
+
+    args = parser.parse_args([TESTFONT_PATH_1])
+    contours_run(args)
+
+    captured = capsys.readouterr()
+    # must be in a tty to get ANSI color output
+    # this is mocked above
+    assert "[ \x1b[1;36m.notdef\x1b[0m ]: 0" in captured.out
+    assert "[ \x1b[1;36mspace\x1b[0m ]: 0" in captured.out
+    assert "[ \x1b[1;36mcomma\x1b[0m ]: 1" in captured.out
+    assert "[ \x1b[1;36mquestion\x1b[0m ]: 2" in captured.out
+    assert "[ \x1b[1;36mA\x1b[0m ]: 2" in captured.out
+    assert "[ \x1b[1;36muni2E2E\x1b[0m ]: 2" in captured.out
+
+
+def test_contours_run_multi_glyph_composite_nocolor(capsys, monkeypatch):
+    def mock_isatty():
+        return True
+
+    # apply the monkeypatch for sys.stdout.isatty()
+    monkeypatch.setattr(sys.stdout, "isatty", mock_isatty)
+
+    args = parser.parse_args(["--nocolor", TESTFONT_PATH_1])
+    contours_run(args)
+
+    captured = capsys.readouterr()
+    # must be in a tty to get ANSI color output
+    # this is mocked above
+    assert "[ .notdef ]: 0" in captured.out
+    assert "[ space ]: 0" in captured.out
+    assert "[ comma ]: 1" in captured.out
+    assert "[ question ]: 2" in captured.out
+    assert "[ A ]: 2" in captured.out
+    assert "[ uni2E2E ]: 2" in captured.out
